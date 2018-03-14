55be98b52f13b289268d09c2cf0914c61e5947d1
Huascar <huascarmc@hotmail.com>
2018-03-14 11:47:53 +0000

    added script to run game, user gets prompted

--- src/game.js
+++ src/game.js
@@ -10,7 +10,7 @@
 
 import {Board} from './board';
 
-class Game{
+export class Game{
   constructor (numberOfRows, numberOfColumns, numberOfBombs){
     this._board = new Board (numberOfRows, numberOfColumns, numberOfBombs);
   }
@@ -19,9 +19,11 @@ playMove(rowIndex, columnIndex){
   this._board.flipTile(rowIndex, columnIndex);
   if (this._board.playerBoard[rowIndex][columnIndex] === 'B'){
     console.log('Game Over!');
+    return 'finished';
     return this._board.print();
   } else if (!this._board.hasSafeTiles()){
     console.log('Congratulations')
+    return 'finished'
   } else {
     console.log('Current Board:');
     this._board.print();}
